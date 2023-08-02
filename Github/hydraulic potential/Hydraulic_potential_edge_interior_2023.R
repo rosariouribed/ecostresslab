e906c5384e58a5505f08b86efe9dcf89787bd48d
nathalia-potter <nathalia.potter@yale.edu>
2023-08-02 14:37:22 -0400

    Update Hydraulic_potential_edge_interior_2023.R

--- Github/hydraulic potential/Hydraulic_potential_edge_interior_2023.R
+++ Github/hydraulic potential/Hydraulic_potential_edge_interior_2023.R
@@ -2,10 +2,12 @@
 
 ### SET DIRECTORY
 getwd()
-path = "F:/Yale/Potencial Hidrico"
+path = "D:/Yale/Potencial Hidrico"
 
 ### IMPORT LIBRARIES
 install.packages('ggridges')
+install.packages("lubridate")
+library(lubridate)
 library(tidyr)
 library(ggplot2)
 library(cowplot)
@@ -16,9 +18,12 @@ library(dplyr)
 library(ggridges)
 
 ### IMPORT DATA
-hyd_pot_23 <- read.csv(file.path(path, "Potencial_Borda_Interior_Novo_20230727_2.csv"))
+hyd_pot_23 <- read.csv(file.path(path, "Potencial_Especies_Compartilhadas.csv"))
 
-hyd_pot_test <- read.csv(file.path(path, "Potencial_Borda_Interior_Novo_20230727_Original.csv"))
+hyd_pot_23_2 <- hyd_pot_23 %>%
+  mutate(dateR = dmy(as.character(Data)))
+
+hyd_pot_test <- read.csv(file.path(path, "Potencial_Borda_Interior_Original_Compartilhadas.csv"))
 
 
 hyd_pot_T <- hyd_pot_test %>%
@@ -26,9 +31,10 @@ hyd_pot_T <- hyd_pot_test %>%
   spread(periodo, meanlwp_manha) %>%
   spread(periodo.1, meanlwp_tarde) %>%
   mutate(delta = tarde - Manha,
-         delta_perc = delta/Manha)
-  ungroup() 
-  
+         delta_perc = delta/Manha) %>%
+  mutate(dateR = dmy(as.character(Data)))
+ungroup() 
+
 #hyd_pot_T %>%
 #  count(Especie_Nome_Comum) %>%
 #  ggplot(aes(x = reorder(Especie_Nome_Comum, n),
@@ -36,76 +42,76 @@ hyd_pot_T <- hyd_pot_test %>%
 #  geom_col() + 
 #  theme_minimal()+
 #  coord_flip()
-  
- hyd_pot_T %>%
-    filter(!is.na(delta_perc)) %>%
-    ggplot(aes(x = reorder(Especie_Nome_Comum, delta_perc),
-               fill = Especie_Nome_Comum,
-               y = delta_perc)) +
-    geom_col(position = "dodge") +
-    #scale_color_continuous(values)
-    theme_minimal()+
-    coord_flip()
- 
- hyd_pot_T %>%
-   filter(!is.na(Manha)) %>%
-   ggplot(aes(x = reorder(Especie_Nome_Comum, Manha),
-              fill = Manha,
-              y = Manha)) +
-   geom_col(position = "dodge") +
-   #scale_color_continuous(values)
-   theme_minimal()+
-   coord_flip()+
-   labs(y="Hydraulic Potential in the Morning (MPa)",
-        x="Species")
- 
- hyd_pot_T %>%
-   filter(!is.na(tarde)) %>%
-   ggplot(aes(x = reorder(Especie_Nome_Comum, tarde),
-              fill = tarde,
-              y = tarde)) +
-   geom_col(position = "dodge") +
-   #scale_color_continuous(values)
-   theme_minimal()+
-   coord_flip()+
-   labs(y="Hydraulic Potential in the Afternoon (MPa)",
-        x="Species")
- 
- hyd_pot_T %>%
-   ggplot(aes(x = reorder(Especie_Nome_Comum, delta),
-              fill = delta,
-              y = delta)) +
-   geom_col(position = "dodge") +
-   #scale_color_continuous(values)
-   theme_minimal()+
-   coord_flip()+
-   labs(y="Difference in Hydraulic Potential (MPa)",
-        x="Species")
- 
- hyd_pot_T %>%
-   gather(var2, values2, Manha:tarde) %>%
-   ggplot(aes(x = reorder(Especie_Nome_Comum, values2),
-              fill = var2,
-              y = values2)) +
-   geom_col(position = "dodge") +
-   #scale_color_continuous(values)
-   theme_minimal()+
-   coord_flip()+
-   labs(y="Mean Hydraulic Potential (MPa)",
-        x="Species")
 
 hyd_pot_T %>%
-   #filter(!is.na(delta_perc)) %>%
-   ggplot(aes(x = Manha,
-              color = Especie_Nome_Comum,
-              y = tarde)) +
-   geom_point() +#scale_color_continuous(values)
-   geom_smooth(method = "lm", se = F) +
-   geom_abline(intercept = 0, slope = 1, color = "red", linetype = 2) +
-   theme_minimal()+
-   ylim(0, 3) +
-   xlim(0, 3) +
-   coord_flip()
+  filter(!is.na(delta_perc)) %>%
+  ggplot(aes(x = reorder(Especie_Nome_Comum, delta_perc),
+             fill = Especie_Nome_Comum,
+             y = delta_perc)) +
+  geom_col(position = "dodge") +
+  #scale_color_continuous(values)
+  theme_minimal()+
+  coord_flip()
+
+hyd_pot_T %>%
+  filter(!is.na(Manha)) %>%
+  ggplot(aes(x = reorder(Especie_Nome_Comum, Manha),
+             fill = Manha,
+             y = Manha)) +
+  geom_col(position = "dodge") +
+  #scale_color_continuous(values)
+  theme_minimal()+
+  coord_flip()+
+  labs(y="Hydraulic Potential in the Morning (MPa)",
+       x="Species")
+
+hyd_pot_T %>%
+  filter(!is.na(tarde)) %>%
+  ggplot(aes(x = reorder(Especie_Nome_Comum, tarde),
+             fill = tarde,
+             y = tarde)) +
+  geom_col(position = "dodge") +
+  #scale_color_continuous(values)
+  theme_minimal()+
+  coord_flip()+
+  labs(y="Hydraulic Potential in the Afternoon (MPa)",
+       x="Species")
+
+hyd_pot_T %>%
+  ggplot(aes(x = reorder(Especie_Nome_Comum, delta),
+             fill = delta,
+             y = delta)) +
+  geom_col(position = "dodge") +
+  #scale_color_continuous(values)
+  theme_minimal()+
+  coord_flip()+
+  labs(y="Difference in Hydraulic Potential (MPa)",
+       x="Species")
+
+hyd_pot_T %>%
+  gather(var2, values2, Manha:tarde) %>%
+  ggplot(aes(x = reorder(Especie_Nome_Comum, values2),
+             fill = var2,
+             y = values2)) +
+  geom_col(position = "dodge") +
+  #scale_color_continuous(values)
+  theme_minimal()+
+  coord_flip()+
+  labs(y="Mean Hydraulic Potential (MPa)",
+       x="Species")
+
+hyd_pot_T %>%
+  #filter(!is.na(delta_perc)) %>%
+  ggplot(aes(x = Manha,
+             color = Especie_Nome_Comum,
+             y = tarde)) +
+  geom_point() +#scale_color_continuous(values)
+  geom_smooth(method = "lm", se = F) +
+  geom_abline(intercept = 0, slope = 1, color = "red", linetype = 2) +
+  theme_minimal()+
+  ylim(0, 3) +
+  xlim(0, 3) +
+  coord_flip()
 
 hyd_pot_T %>%
   select(Placa:tarde) %>%
@@ -122,9 +128,9 @@ hyd_pot_T %>%
 ggplot(hyd_pot_T, aes(x=Data, y=Manha, group=Especie_Nome_Comum, color=Especie_Nome_Comum))+
   geom_line(lwd=0.8)+
   theme_classic()
-  
-  
-ggplot(hyd_pot_23, aes(x = Data, y = mean_pot, group=Especie_Nome_Comum, color=Especie_Nome_Comum)) +
+
+
+ggplot(hyd_pot_23_2, aes(x = dateR, y = mean_pot, group=Especie_Nome_Comum, color=Especie_Nome_Comum)) +
   geom_line(lwd=0.8) +
   facet_grid(fct_inorder(Periodo)~Local)+
   theme_minimal()+
@@ -137,30 +143,24 @@ ggplot(hyd_pot_23, aes(x = Data, y = mean_pot, group=Especie_Nome_Comum, color=E
                                 margin = margin(t=0,r=30,b=0,l=0)),
     axis.text = element_text(size=8, color='black')
   )
-  
-hyd_pot_23 %>%
-    ggplot(aes(x = Data, y = mean_pot, group=Periodo, color=Periodo)) +
-   stat_summary(fun.data = "mean_cl_boot", alpha = .8,
-               linewidth = 2, size = 3, geom = "ribbon") +
-   facet_grid(~Local)+
-    theme_minimal()+
-  labs(y="Mean Hydraulic Potential (MPa)",
-       x="Date")
 
-
-hyd_pot_23 %>%
-  ggplot(aes(x = Data, y = mean_pot, group=Local, color=Local)) +
+hyd_pot_23_2 %>%
+  ggplot(aes(x = dateR, y = mean_pot, group=Periodo, color=Periodo)) +
   stat_summary(fun.data = "mean_cl_boot", alpha = .5,
                linewidth = 2, size = 3, geom = "ribbon") +
-  facet_grid(~Periodo)+
+  facet_grid(~Local)+
   theme_minimal()+
   labs(y="Mean Hydraulic Potential (MPa)",
        x="Date")
 
 
-
-
-
-
+hyd_pot_23_2 %>%
+  ggplot(aes(x = dateR, y = mean_pot, group=Local, color=Local)) +
+  stat_summary(fun.data = "mean_cl_boot", alpha = .3,
+               linewidth = 2, size = 3, geom = "ribbon") +
+  facet_grid(~Periodo)+
+  theme_minimal()+
+  labs(y="Mean Hydraulic Potential (MPa)",
+       x="Date")
 
 
